96405cbbfbd7cde1b3280e2a0ad85029a65a2800
Daron Dierkes <ddierkes@mohistory.org>
2018-09-12 14:03:53 -0500

    filtering

--- pages/institutions.vue
+++ pages/institutions.vue
@@ -8,7 +8,7 @@
     <v-toolbar-title>State selection</v-toolbar-title>
     <v-autocomplete
       :loading="loading"
-      :items="items"
+      :items="states"
       :search-input.sync="search"
       v-model="select"
       cache-items
@@ -16,18 +16,17 @@
       flat
       hide-no-data
       hide-details
-      label="What state are you traveling?"
+      label="Where are you traveling?"
       solo-inverted
     ></v-autocomplete>
-    <v-btn icon>
+    <!--<v-btn icon>
       <v-icon>more_vert</v-icon>
-    </v-btn>
+    </v-btn>-->
   </v-toolbar>
     <v-card>
-      {{data.length}}
       <GmapMap
-            :center="{lat:38, lng:-100}"
-            :zoom="4"
+            :center="center"
+            :zoom="zoom"
             map-type-id="terrain"
             style="height: 300px "
           >
@@ -47,47 +46,45 @@
           </GmapMap>
 
         <v-list three-line>
-          <template v-for="(item, index) in items">
-            <v-subheader
-              v-if="item.header"
-              :key="item.header"
-            >
-              {{ item.header }}
-            </v-subheader>
+          <v-subheader>
+            Participating Institutions: {{data.length || 0}} found.  {{select}}
+          </v-subheader>
+          <template v-for="(item, index) in data">
 
-            <v-divider
+
+            <!--<v-divider
               v-else-if="item.divider"
               :inset="item.inset"
               :key="index"
-            ></v-divider>
+            ></v-divider>-->
+            <v-divider></v-divider>
 
             <v-list-tile
-              v-else
-              :key="item.title"
+              :key="item.id"
               avatar
               @click=""
             >
-              <v-list-tile-avatar size="60" tile class="pr-3" >
-                <img :src="item.avatar">
+              <v-list-tile-avatar v-if="item.Image_Thumb[0]" size="60" tile class="pr-3" >
+                <img :src="item.Image_Thumb[0]">
               </v-list-tile-avatar>
 
               <v-list-tile-content>
-                <v-list-tile-title v-html="item.title"></v-list-tile-title>
-                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
-                <v-list-tile-sub-title v-html="item.address"></v-list-tile-sub-title>
+                <v-list-tile-title v-html="item.Title"></v-list-tile-title>
+                <v-list-tile-sub-title v-html="item.Address_Street"></v-list-tile-sub-title>
+                <a :href="item.URL"><v-list-tile-sub-title v-html="item.URL"></v-list-tile-sub-title></a>
               </v-list-tile-content>
             </v-list-tile>
           </template>
         </v-list>
       </v-card>
-       <div class="text-xs-center mb-4 mt-3">
+       <!--<div class="text-xs-center mb-4 mt-3">
         <v-pagination
           v-model="page"
           :length="15"
           :total-visible="7"
         >
         </v-pagination>
-      </div>
+      </div>-->
     </v-flex>
   </v-layout>
 </template>
@@ -98,58 +95,10 @@
     data () {
       return {
         data: {},
-        items: [
-          { header: 'Participating Institutions' },
-          {
-            avatar: 'http://timetravelers.mohistory.org/sites/default/files/styles/mhm_blocks_rotor/public/Rogers%20historical%20society%20exterior.jpg?itok=Qa535RZX',
-            title: 'ROGERS HISTORICAL MUSEUM',
-            subtitle: 'Gift Shop Discount Special Event Offer',
-            address: '1123 Grape St. Louis, MO 63103'
-          },
-          { divider: true, inset: true },
-          {
-            avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-            title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-            subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
-          },
-          { divider: true, inset: true },
-          {
-            avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-            title: 'Oui oui',
-            subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-          },
-          { divider: true, inset: true },
-          {
-            avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
-            title: 'Birthday gift',
-            subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?"
-          },
-          { divider: true, inset: true },
-          {
-            avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-            title: 'Recipe to try',
-            subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
-          }
-        ],
-        markers: [
-          {
-            position: { lat: 38, lng: -90 },
-            name: 'stl',
-            infoText: 'Marker 1'
-          },
-          {
-            position: { lat: 30, lng: -110 },
-            name: 'over there',
-            infoText: 'Marker 2'
-          },
-          {
-            position: { lat: 42, lng: -88 },
-            name: 'over the other way',
-            infoText: 'Marker 3'
-          }
-        ],
+        fullData: {},
+        zoom: 4,
         icon: {url: '/location.png'},
-        infoContent: 'fhggfhgfdhgfdh',
+        infoContent: 'place holder text',
         infoWindowPos: { lat: 30, lng: -110 },
         infoWinOpen: false,
         infoOptions: {
@@ -165,7 +114,6 @@
         states: [
           'Alabama',
           'Alaska',
-          'American Samoa',
           'Arizona',
           'Arkansas',
           'California',
@@ -173,7 +121,6 @@
           'Connecticut',
           'Delaware',
           'District of Columbia',
-          'Federated States of Micronesia',
           'Florida',
           'Georgia',
           'Guam',
@@ -202,7 +149,6 @@
           'New York',
           'North Carolina',
           'North Dakota',
-          'Northern Mariana Islands',
           'Ohio',
           'Oklahoma',
           'Oregon',
@@ -222,12 +168,67 @@
           'West Virginia',
           'Wisconsin',
           'Wyoming'
-        ]
+        ],
+        center: {lat: 38, lng: -100},
+        stateCenters: {
+          'Alabama': {lat: 32.806671, lng: -86.791130},
+          'Alaska': {lat: 61.37071, lng: -152.404419},
+          'Arizona': {lat: 33.72975, lng: -111.431221},
+          'Arkansas': {lat: 34.96970, lng: -92.373123},
+          'California': {lat: 36.11620, lng: -119.681564},
+          'Colorado': {lat: 39.05981, lng: -105.311104},
+          'Connecticut': {lat: 41.59778, lng: -72.755371},
+          'Delaware': {lat: 39.31852, lng: -75.507141},
+          'District of Columbia': {lat: 38.89743, lng: -77.026817},
+          'Florida': {lat: 27.76627, lng: -81.686783},
+          'Georgia: ': {lat: 33.04061, lng: -83.643074},
+          'Hawaii': {lat: 21.09431, lng: -157.498337},
+          'Idaho': {lat: 44.24045, lng: -114.478828},
+          'Illinois': {lat: 40.34945, lng: -88.986137},
+          'Indiana': {lat: 39.84942, lng: -86.258278},
+          'Iowa': {lat: 42.01153, lng: -93.210526},
+          'Kansas': {lat: 38.52660, lng: -96.726486},
+          'Kentucky': {lat: 37.66814, lng: -84.670067},
+          'Louisiana': {lat: 31.16954, lng: -91.867805},
+          'Maine': {lat: 44.69394, lng: -69.381927},
+          'Maryland': {lat: 39.06394, lng: -76.802101},
+          'Massachusetts': {lat: 42.23017, lng: -71.530106},
+          'Michigan': {lat: 43.32661, lng: -84.536095},
+          'Minnesota': {lat: 45.69445, lng: -93.900192},
+          'Mississippi': {lat: 32.74164, lng: -89.678696},
+          'Missouri': {lat: 38.45608, lng: -92.288368},
+          'Montana': {lat: 46.92192, lng: -110.454353},
+          'Nebraska': {lat: 41.12537, lng: -98.268082},
+          'Nevada': {lat: 38.31351, lng: -117.055374},
+          'New Hampshire': {lat: 43.45249, lng: -71.563896},
+          'New Jersey': {lat: 40.29890, lng: -74.521011},
+          'New Mexico': {lat: 34.84051, lng: -106.248482},
+          'New York': {lat: 42.16572, lng: -74.948051},
+          'North Carolina': {lat: 35.63006, lng: -79.806419},
+          'North Dakota': {lat: 47.52891, lng: -99.784012},
+          'Ohio': {lat: 40.38878, lng: -82.764915},
+          'Oklahoma': {lat: 35.56534, lng: -96.928917},
+          'Oregon': {lat: 44.57202, lng: -122.070938},
+          'Pennsylvania': {lat: 40.59075, lng: -77.209755},
+          'Rhode Island': {lat: 41.68089, lng: -71.511780},
+          'South Carolina': {lat: 33.85689, lng: -80.945007},
+          'South Dakota': {lat: 44.29978, lng: -99.438828},
+          'Tennessee': {lat: 35.74784, lng: -86.692345},
+          'Texas': {lat: 31.05448, lng: -97.563461},
+          'Utah': {lat: 40.15003, lng: -111.862434},
+          'Vermont': {lat: 44.04587, lng: -72.710686},
+          'Virginia': {lat: 37.76933, lng: -78.169968},
+          'Washington': {lat: 47.40090, lng: -121.490494},
+          'West Virginia': {lat: 38.49122, lng: -80.954453},
+          'Wisconsin': {lat: 44.26854, lng: -89.616508},
+          'Wyoming': {lat: 42.75596, lng: -107.302490}
+        }
       }
     },
     watch: {
       search (val) {
         val && val !== this.select && this.querySelections(val)
+        this.filterData()
       }
     },
     created: function () {
@@ -255,6 +256,17 @@
           this.currentMidx = idx
         }
       },
+      filterData: function () {
+        let self = this
+        self.data = []
+        for (let i = 0; i < self.fullData.length; i++) {
+          if (self.fullData[i].Address_State === self.select) {
+            self.data.push(self.fullData[i])
+          }
+        }
+        this.center = this.stateCenters[this.select]
+        this.zoom = 5
+      },
       getPoints: function () {
         let path = 'http://csv.mohistory.org/json/institutions'
         let self = this
@@ -264,6 +276,7 @@
           })
           .then(function (myJson) {
             self.data = myJson.data
+            self.fullData = myJson.data
             console.log(self.data)
           })
           .catch(function (error) {
