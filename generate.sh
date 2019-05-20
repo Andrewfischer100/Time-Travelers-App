74fd0a7ba6235691b5c9d124284a005d51d10216
themohistorylab <themohistorylab@mohistory.org>
2019-05-20 20:16:08 +0000

    modifed the generate script

--- generate.sh
+++ generate.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-export PATH=$PATH:/home/weboffice/.nvm/versions/node/v10.15.1/bin
-
-. ~/.nvm/nvm.sh
-nvm use default
 
 cd /var/www/vhosts/timetravelers.mohistory.org/timetravelersapp
 
-/home/weboffice/.nvm/versions/node/v10.15.1/bin/npm run generate
+/usr/bin/npm run generate
+rm -rf dist2
+cp -r dist dist2
 
 
