27c02ea538389274b008a63b9e49f0057f03fa69
themohistorylab <themohistorylab@mohistory.org>
2019-05-20 20:23:01 +0000

    http to https

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,7 +4,7 @@
     <v-flex xs12 sm10 offset-sm1>
       <v-card class="elevation-5">
         <div class="mb-0 text-xs-center">  
-          <iframe class="mt-5" src="http://admin.mohistory.org/forms/tt/contact.html">
+          <iframe class="mt-5" src="https://admin.mohistory.org/forms/tt/contact.html">
             <p>Your browser does not support iframes.</p>
           </iframe>
           </div>
